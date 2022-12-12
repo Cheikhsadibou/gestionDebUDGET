e507d810499bf97f1067bb96ae119c2434af2405
Cheikhsadibou <111586764+Cheikhsadibou@users.noreply.github.com>
2022-12-12 13:49:17 +0000

    Delete revision.py

--- revision.py
+++
@@ -1,7 +0,0 @@
-
-import copy
-
-inventaire = ["marteau","ras clou","point", "brique", "tenaille", "brique", "siseau"]
-
-for k, v in enumerate(inventaire):
-  print("l'indice de l'element {} --> {}".format(k, v))
